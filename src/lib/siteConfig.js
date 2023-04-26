25df3f1180c996c48a80b3596c3754c9b0733d93
Michael Schienbein <michael.schienbein@fhr.com>
2023-04-26 18:23:20 -0500

    test post

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -7,7 +7,7 @@ export const DEFAULT_OG_IMAGE =
 	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
 export const MY_TWITTER_HANDLE = 'mschien_';
 export const MY_YOUTUBE = 'https://youtube.com/fact0';
-export const POST_CATEGORIES = ['Tutorial', 'Note', 'Essay', 'Tips']; // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
+export const POST_CATEGORIES = ['Tutorial', 'Note', 'Essay', 'Tips', 'Blog']; // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
 export const GH_PUBLISHED_TAGS = ['Published'];
 
 // auto generated variables
