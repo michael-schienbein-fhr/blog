ba0495f1e1facd8b3d355aae48b7525003a216df
Michael Schienbein <michael.schienbein@fhr.com>
2023-04-26 11:56:17 -0500

    some config

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -1,13 +1,13 @@
-export const SITE_URL = 'https://swyxkit.netlify.app';
-export const APPROVED_POSTERS_GH_USERNAME = ['sw-yx'];
-export const GH_USER_REPO = 'sw-yx/swyxkit'; // used for pulling github issues and offering comments
-export const SITE_TITLE = 'SwyxKit';
-export const SITE_DESCRIPTION = "swyx's default SvelteKit + Tailwind starter";
+export const SITE_URL = 'https://schienbeinblog.netlify.app/';
+export const APPROVED_POSTERS_GH_USERNAME = ['michael-schienbein-fhr'];
+export const GH_USER_REPO = 'michael-schienbein-fhr/blog'; // used for pulling github issues and offering comments
+export const SITE_TITLE = "Michael Schienbein's Blog";
+export const SITE_DESCRIPTION = "Learning in public while creating tutorials based on things I'm experimenting with.";
 export const DEFAULT_OG_IMAGE =
 	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
-export const MY_TWITTER_HANDLE = 'swyx';
-export const MY_YOUTUBE = 'https://youtube.com/swyxTV';
-export const POST_CATEGORIES = ['Essay', 'Note']; // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
+export const MY_TWITTER_HANDLE = 'mschien_';
+// export const MY_YOUTUBE = 'https://youtube.com/fact0';
+export const POST_CATEGORIES = ['Tutorial', 'Note', 'Essay', 'Tips']; // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
 export const GH_PUBLISHED_TAGS = ['Published'];
 
 // auto generated variables
